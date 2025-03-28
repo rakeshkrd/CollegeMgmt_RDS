8c046fc3e183da1fbf7a80834cd9b616a3601142
rakeshkrd <131772160+rakeshkrd@users.noreply.github.com>
2025-03-28 14:46:36 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'cmtsdb.cv0s6okuqdhj.ap-south-1.rds.amazonaws.com',
+	'hostname' => 'database-123.c3kg6eqyukgx.ap-south-1.rds.amazonaws.com',
 	'username' => 'admin',
 	'password' => 'admin123',
 	'database' => 'college_mgmt',
